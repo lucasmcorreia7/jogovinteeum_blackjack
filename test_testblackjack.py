60f352955090f862f443eb88811733f490c06e4d
lucasmcorreia7 <74879641+lucasmcorreia7@users.noreply.github.com>
2021-06-03 13:54:36 -0300

    Update test_testblackjack.py

--- test_testblackjack.py
+++ test_testblackjack.py
@@ -1,5 +1,7 @@
 
 
+
+
 __created__= 'By Lucas M.Correia'
 __license__= 'FREE'
 '''
@@ -24,29 +26,34 @@ values=  {
     "Rainha": 10,
     "Rei": 10,
     "AS": 11
-{    #  dictionary
+}  # dictionary
 playing = True   #  bool
 
 
-#  criando a classe cartas
+#  create a card class
 class Card():
-    def __init__(self):
+    def __init__(self, suit, rank):
         self.suit = suit
-        self.ranking = ranking
+        self.rank = rank
 
     def __str__(self):
+        return      self.rank + " of " + self.suit
+
+
+# create deck class
+class Deck():
+    def __init__(self):
         self.deck= []
         for suit in suits:
-            for ranking in ranking:
-          self.deck.append(Card(suit, ranking) #append the card object to self.deck list
+            for rank in ranking:
+                self.deck.append(Card(suit,  rank))  # appending the Card object to self.deck list
+    def __str__(self):
+        deckComposition = "" # set the deck comp as an empty string
+        for card in self.deck:
+            deckComposition += '\n' + card.__str__()  # Card class string representation
+            return "The deck has : " + deckComposition
 
-    def __dir__(self):
-        DeckComposition = ''   #set the deck comp as an empty string
-          for card in self.deck:
-               deckComposition  += '/n'  + card.__str__()  # Card class string representation
-            return  " the deck has:  " + deckComposition
-
-    def shuffle (self):
+    def shuffle(self):
         random.shuffle(self.deck)
 
         # grab the deck attribute of Deck class then pop off card item from list and set it to single card
@@ -70,52 +77,52 @@ class Hand():
 
          # card object is Deck.deal() that has single card and sc has suit and rank
          def addCard(self, card):
-             self.cards.append(card):  # talking card object and appending it to self.cards list
-             # take cards ranking and lookup in values dict and add to self.value
-             self.value[card.rank]
+             self.cards.append(card)  # taking card object and appending it to self.cards list
+             # take the cards rank and lookup in values dict and add to self.value
+             # if value is > 21 this particular hand has lost the game
+             self.value += values[card.rank]
 
             # track aces
-             if card.rank == 'AS'
+             if card.rank == 'Ás':
               self.aces += 1
 
          # test_deck = Deck()
-         #test_deck.shuffle()
+         # test_deck.shuffle()
          # test_player = Hand()
-         # retirar_cartas = test_deck.deal()
-         # print(retirar_cartas)
-         # test_ player.addCartas(retirar_cartas)
-         # print (test_player.value)
-         def checkAS(self):
+         # pulled_card = test_deck.deal()
+         # print(pulled_card)
+         # test_player.addCard(pulled_card)
+         # print(test_player.value)
          # if value > 21 and if ace is available
-        while self.value > 21 and self.AS: # we can also set this to > 0
-            self.value -= 10 # take my value and subtract 10
-            self.aces -= 1 #   take my aces and subtract 1
+     while self.value > 21 and self.aces:  # we can also set this to > 0
+         self.value -= 10  # take my value and subtract 10
+         self.aces -= 1  # take my aces and subtract 1
 
 # create chips class
 class Chips():
-      def __init__(self,  total=1000):
-      self.total = total # set a default value or allow user to input
-      self.bet= 0
+    def __init__(self, total=1000):
+        self.total = total  # set a default value or allow user to input
+        self.bet = 0
 
-     def winBet(self):
-     self.total += self.bet
+    def winBet(self):
+        self.total += self.bet
 
-     def loseBet(self):
-         self.total -= self.bet
+    def loseBet(self):
+        self.total -= self.bet
 
 
 # create a function for talking bets
 def takeBet(chips):
-     while True:
-         try:
-            chips.bet = int(input("Quantas você deseja apostar?"))
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet? "))
         except ValueError:
-           print("Desculpe-me, você precisa digitar uma aposta representada por algum número!")
+            print('Sorry, a bet must be a number!')
         else:
-           of chips.bet > chips.total:
-              print("Desculpe, sua aposta não pode ser maior que seu número de fichas {}".format(chips.total))
-        else:
-            break
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed, you have only {} chips left.".format(chips.total))
+            else:
+                break
 
 
 # function for taking hits
@@ -132,48 +139,46 @@ def hitOrStand(deck, hand):
     global playing
 
     while True:
-        X = input("Você gostaria de mais uma carta ou parar? Digite 'h' or 's': "
+        x = input("Would you like to Hit or Stand? Enter 'h' or 's': ")
 
-        of x[0].lower() == 'h':
-       hit(deck, hand)
-       elif x [0].lower() == 'h':
-           hit (deck, hand)
-    elif x[0].lower() == 's':
-        print("O jogador está de pé. O dealer está jogando.")
-        plating = False4
-    else:
-         print("Desculpe, tente outra vez")
-        continue
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing.")
+            playing = False4
+        else:
+            print("Sorry, try again!")
+            continue
 
-     break
+        break
 
 
 # function for showing cards
 def showCards(player, dealer):
-print("/nTurno do nDealers:  ")
-print( "<carta escondida>")
- # print (' ' + dealer.cards[1].__str__())
-   print ("\nPlayer's Turno"),  *players.card, sep=' \n ' )
+    print("\nDealers's Turn: ")
+    print("<card hidden>")
+    # print(' ' + dealer.cards[1].__str__())
+    print('', dealer.cards[1])
+    print("\nPlayer's Turn:", *player.cards, sep='\n ')
 
 
 def showAll(player, dealer):
-    # asterisk + symbol is used to print evert item in a collection
-    print ("/ nDealer's Turno: " , *dealer.cards, sep= '\n ') # sep 'n' argument prints each item on a separate line
-    print(" Dealer's Turno =" , dealer.value)
-    print("n/Player's Turno:" , *player.cards, sep'\n ')
-    print("Player's Turno = ", player.value)
+    # asterisk * symbol is used to print every item in a collection
+    print("\nDealer's Turn:", *dealer.cards, sep='\n ')  # sep='\n ' argument prints each item on a separate line
+    print("Dealer's Turn =", dealer.value)
+    print("\nPlayer's Turn:", *player.cards, sep='\n ')
+    print("Player's Turn =", player.value)
 
 
 # functions for ending game scenarios
-def PlayerBusted(player, dealer , chips):
-   print("Player busted (arrebenta), perde a aposta")
- chips.loseBet()
+def playerBusted(player, dealer, chips):
+    print("Player busted!")
+    chips.loseBet()
 
 
 def playerWins(player, dealer, chips):
-print("O Jogador ganhou, parabéns! ")
+    print("Player wins!")
     chips.winBet()
-
 
 def dealerBusted(player, dealer , chips):
    print("Dealer busted! , arrebentou sua aposta")
@@ -194,60 +199,56 @@ def greatTie(players, dealer):
 while True:
    print("Bem vindo ao BlackJack")
  # shuffle and deal two cards to each player
-  deck = Deck()
-  deck.shuffle()
+deck = Deck()
+deck.shuffle()
 
-   playerHand = Hand()
-   playerHand.addCard(deck.deal())
-  playerHand.addCard(deck.deal())
+dealerHand = Hand()
+dealerHand.addCard(deck.deal())
+dealerHand.addCard(deck.deal())
 
- dealerHand = Hand()
- dealerHand.addcard(deck.deal())
-  dealerHand.addCard(deck.deal())
+playerChips = Chips()  # players chips
+takeBet(playerChips)  # prompting player for their bet
 
-    playerChips = Chips()  # player chips
-    takeBet(playerChips)  # prompting player for their bet
+showCards(playerHand, dealerHand)  # show cards for dealer and player
 
-   showCard(playerHand, dealerHand)  # show cards for dealer and player
+while playing:
+    hitOrStand(deck, playerHand)  # prompt for hit or stand
 
-   while playing:
-       hitOrStand(deck, playerHand)  # prompt for hit or stand
+    showCards(playerHand, dealerHand)
 
-      showCards(playerHand, dealerHand)
+    # if player hand exceeds 21 run playerBusted() and break
+    if playerHand.value > 21:
+        playerBusted(playerHand, dealerHand, playerChips)
 
-        # if player hand exceeds 21 run playerBusted() and break
-     if playerHand.value > 21:
-       playerBusted(playerHand, dealerHand, playerChips)
-
-       break
+        break
 
 # soft 17 rule: keep playing until dealer reaches 17
-   if playerHand.value <= 21:
+if playerHand.value <= 21:
     while dealerHand.value < playerHand.value:
-       hit(deck, dealerHand)
+        hit(deck, dealerHand)
 
-    showAll(playerHand, dealerHand)     # show all cards
+    showAll(playerHand, dealerHand)  # show all cards
 
     # run a different game win scenario
-    if dealer.Hand.value  > 21:
-       dealerBusted(playerHand, dealerHand, PlayerChips)
+    if dealerHand.value > 21:
+        dealerBusted(playerHand, dealerHand, playerChips)
     elif dealerHand.value > playerHand.value:
-       dealerWins(PlayerHand, dealerHand, playerChips)
-    elif dealerHand.value  < playerHand.value:
-       playerWins(playerHand, dealerHand, playerChips)
-    elif dealerHand.value < playerHand.value :
-     playerHand(playerHand,  dealerHand, playerChips)
-   else:
-      greatTie(playerHand,  dealerhand)
+        dealerWins(playerHand, dealerHand, playerChips)
+    elif dealerHand.value < playerHand.value:
+        playerWins(playerHand, dealerHand, playerChips)
+    else:
+        greatTie(playerHand, dealerHand)
 
 # show player their total remaining chips
-print("\nPlayer total chips estão em: {}".format(playerChips.total))
+print("\n Player total chips are at: {}".format(playerChips.total))
 
- # ask to play again
-newGame = input("Quer jogar de novo? S / n:")
+# ask to play again
+newGame = input("Want to play again? y/n: ")
 
-if newGame[0].lower() == 'y':
-    playing = True
-    continue
-else: print"Obrigado por jogar BlackJack! espero ver você novamente.")
-    break
+      if newGame[0].lower() == 'y':
+          playing = True
+          continue
+      else:
+           print("Thank you for playing BlackJack! See you again.")
+           break
+
